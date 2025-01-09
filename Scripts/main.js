c6493a56ac74af7053e2baccd7ff9a7fc66045a0
Wiz The Bard <72505202+jvan1234@users.noreply.github.com>
2025-01-09 15:56:45 -0500

    changed some things around

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     // Find current background image
-    var curImage = toString($("body").css("background-image"));
+    var curImage = $("body").css("background-image");
 
     console.log(curImage + " is the current background image...");
-    if (curImage == "/Images/blackCheckers.png") {
+    if (curImage == "url(/Images/blackCheckers.png)") {
         console.log("It is currently the dark theme!");
     }
 })
