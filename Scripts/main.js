29a7f2801c2ffd3aaa50c661a4fa9faa280068ed
Wiz The Bard <72505202+jvan1234@users.noreply.github.com>
2025-01-09 16:21:39 -0500

    Reading! (and colows :D)

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,9 +1,14 @@
+function changeThemes(String theme)
+{
+
+}
+
 $(document).ready(function(){
     // Find current background image
     var curImage = $("body").css("background-image");
 
     console.log(curImage + " is the current background image...");
     if (curImage == "url(\"https://jvan1234.github.io/Images/blackCheckers.png\")") {
-        console.log("It is currently the dark theme!");
+        $("body").find("#regularNormal").css("color: white");
     }
 })
