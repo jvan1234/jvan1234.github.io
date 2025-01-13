5f01c91202616e2758ccabd21a7cbbf7ca7a1529
Wiz The Bard <72505202+jvan1234@users.noreply.github.com>
2025-01-13 15:27:07 -0500

    Allowed Titles to be changed. Made color change a seperate function

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,11 @@
+function changeBackground(backgroundImage)
+{
+    if (backgroundImage === "https://jvan1234.github.io/Images/blackCheckers.png") {
+        console.log("Image represents the dark mode! Making text white...");
+        $("body").find(".normalRegular").css({"color" : "white"});
+    }
+}
+
 $(document).ready(function(){
     // Find current background image
     var curImage = $("body").css("background-image");
@@ -7,8 +15,5 @@ $(document).ready(function(){
     curImage = curImage.replace('url("','').replace('")','');
 
     console.log(curImage + " is the actual address...");
-    if (curImage === "https://jvan1234.github.io/Images/blackCheckers.png") {
-        console.log("Image represents the dark mode! Making text white...");
-        $("body").find(".normalRegular").css({"color" : "white"});
-    }
+    changeBackground(curImage);
 })
