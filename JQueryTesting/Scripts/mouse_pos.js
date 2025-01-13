0418b3df2e71d93553576f5fc8cec435f7d6070d
Wiz The Bard <72505202+jvan1234@users.noreply.github.com>
2025-01-13 18:31:26 -0500

    maybe?

--- JQueryTesting/Scripts/mouse_pos.js
+++ JQueryTesting/Scripts/mouse_pos.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Mark down events
-    $(document).on("mousemove", function(event){
+    $(document).mousemove(function(event){
         // When the mouse position updates, update an id called mouseDisplay to display where the mouse is found on the page
         $("#mouseDisplay").text("Current Mouse Position: (" + event.pageX + ", " + event.pageY + ")");
     });
