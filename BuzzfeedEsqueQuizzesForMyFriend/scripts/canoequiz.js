5c7ff1a947659afd8af336ad37bc48db762ef26c
Joshua <72505202+jvan1234@users.noreply.github.com>
2026-03-27 21:01:45 -0400

    checking if I got the right shit cuz istg if i didnt

--- BuzzfeedEsqueQuizzesForMyFriend/scripts/canoequiz.js
+++ BuzzfeedEsqueQuizzesForMyFriend/scripts/canoequiz.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+    // Example Answer w/ scores
+    const exampleQuestions = {
+        "(1) Do you like cats or dogs?" : {
+            "Cats" : 10,
+            "Dogs" : 15
+        },
+        "(2) Are you over 18?" : {
+            "Yes" : 1000,
+            "No" : 0
+        }
+    }
+
+    let quizDiv = $("#questionaire");
+
     console.log("Page loaded!");
+
+    console.log(quizDiv.id + " is the class of the questionaire div *hopefully");
 })
