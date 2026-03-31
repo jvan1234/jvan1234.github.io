3f3e068298e727d070aeded85307e2e23ef4d8b3
Joshua <72505202+jvan1234@users.noreply.github.com>
2026-03-31 11:09:34 -0400

    Added question text

--- BuzzfeedEsqueQuizzesForMyFriend/scripts/canoequiz.js
+++ BuzzfeedEsqueQuizzesForMyFriend/scripts/canoequiz.js
@@ -3,22 +3,26 @@ $(document).ready(function(){
     const exampleQuestions = {
         1 : {
             "question" : "(1) Do you like cats or dogs?",
-            "Cats" : 10,
-            "Dogs" : 15
+            "ans" : {
+                "Cats" : 10,
+                "Dogs" : 15
+            }
         },
         2 : {
             "question" : "(2) Are you over 18?",
-            "Yes" : 1000,
-            "No" : 0
+            "ans" : {
+                "Yes" : 1000,
+                "No" : 0
+            }
         }
     }
 
     let quizDiv = $("#questionaire");
-
+    let questionTxt = $("#question");
     console.log("Page loaded!");
 
     console.log(quizDiv.id + " is the class of the questionaire div *hopefully");
-
+    questionTxt.text(exampleQuestions[1].question);
     let btn = $("<button>", {
         "type":"button",
         "onclick":"alert(\"Surprise!\")",
